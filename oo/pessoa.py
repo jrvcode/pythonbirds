7a5ffa0dcfd185972efce80d2db98b044497a211
jrvcode <contato.juniorvieira@gmail.com>
2022-04-16 21:20:13 -0300

    Implmentado carro com direção e aceleração

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,13 @@
 class Pessoa:
+    def __int__(self, *filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
-if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
 
+if __name__ == '__main__':
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
